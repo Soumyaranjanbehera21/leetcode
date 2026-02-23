7126b23c200cc361e9b7f092df88dd837b8fbbeb
Soumyaranjan Behera <srbashish13@gmail.com>
2026-02-24 01:58:04 +0530

    Time: 5 ms (44.49%) | Memory: 13.1 MB (80.52%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashMap = {} # val : index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[n] = i
